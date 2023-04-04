0e8f24023f10964cfb785dd47833e8a5b002fdb1
Ed Herron <eherron325@gmail.com>
2023-04-04 17:32:33 +0100

    javascript working as intended

--- scripts/script.js
+++ scripts/script.js
@@ -5,10 +5,21 @@ function updatePictureMelissa() {
 
 const updatePictureUnited = () => {
     let image = document.getElementById("picture-of-me");
-    image.src = "./media/Manchester-United-logo.png";
+    image.src = "./media/united.jpg";
+    image.style.borderRadius = "0";
+    image.style.border = "none";
+}
+
+const restorePicture = () => {
+    let image = document.getElementById("picture-of-me");
+    image.src = "./media/pic-of-me.jpg";
+    image.style.borderRadius = "100%";
+    image.style.border = "solid 1px black";
 }
 
 let melissa = document.getElementById("my-wife");
 let united = document.getElementById("united");
 melissa.addEventListener("mouseover", updatePictureMelissa);
-united.addEventListener( "click", updatePictureUnited);
+melissa.addEventListener("mouseout", restorePicture);
+united.addEventListener("mouseover", updatePictureUnited);
+united.addEventListener("mouseout", restorePicture);
